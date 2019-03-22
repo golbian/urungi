698b271f869e0065445816881f9eef3ce61c76fe
Marot Florian <marot.florian@hotmail.com>
2019-03-22 15:47:46 +0100

    Indent fix.

--- public/js/report/services/c3Charts.js
+++ public/js/report/services/c3Charts.js
@@ -165,7 +165,7 @@ angular.module('app').service('c3Charts', function () {
             };
         }
 
-          if (report.properties.chart.legendPosition === 'none') {
+        if (report.properties.chart.legendPosition === 'none') {
             c3Config.legend = {
                 hide: true,
             };
